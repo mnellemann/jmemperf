dae001bec4dcd65e6dc642e3c6a045dfc78acc4b
Mark Nellemann <mark.nellemann@gmail.com>
2023-06-29 21:02:27 +0200

    Cleanup and update build.

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # Memory Performance Test
 
+Small tool to test memory write and read performance.
+
+```shell
+Usage: jmemperf [-hV] [-d=NUM] [-i=NUM] [-r=NUM] [-t=NUM]
+Memory performance measurement tool.
+  -d, --data=NUM         Create this much data (MB) pr. row [default: 100]
+  -h, --help             Show this help message and exit.
+  -i, --iterations=NUM   Iterate this many times [default: 3]
+  -r, --rows=NUM         Create this many rows pr. table [default: 10]
+  -t, --tables=NUM       Create this many tables [default: 4]
+  -V, --version          Print version information and exit.
+```
 
 ## Examples
 
@@ -12,3 +24,7 @@ Test with 100GB memory:
 ```shell
 java -Xms128g -Xmx128g -XX:+AlwaysPreTouch -jar memstress-0.0.1-all.jar -t 100
 ```
+
+## Credits
+
+- Jens Ebbesen for improvements.
--- src/main/java/biz/nellemann/jmemperf/Application.java
+++ src/main/java/biz/nellemann/jmemperf/Application.java
@@ -1,3 +1,18 @@
+/*
+   Copyright 2023 mark.nellemann@gmail.com
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
 package biz.nellemann.jmemperf;
 
 import org.slf4j.Logger;
@@ -22,7 +37,7 @@ public class Application implements Callable<Integer> {
     @CommandLine.Option(names = { "-d", "--data" }, paramLabel = "NUM", description = "Create this much data (MB) pr. row [default: ${DEFAULT-VALUE}]")
     int maxDataPerRow = 100;
 
-    @CommandLine.Option(names = { "-i", "--iterations" }, paramLabel = "NUM", description = "Iterate test his many times [default: ${DEFAULT-VALUE}]")
+    @CommandLine.Option(names = { "-i", "--iterations" }, paramLabel = "NUM", description = "Iterate this many times [default: ${DEFAULT-VALUE}]")
     int iterations = 3;
 
     @Override
